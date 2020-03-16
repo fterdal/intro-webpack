844f45aadbe541068446f342486e9ac604a00761
Finn <finnterdal@gmail.com>
2020-03-16 01:38:34 -0500

    make it broken!

--- public/renderMonsters.js
+++ public/renderMonsters.js
@@ -1,19 +1,5 @@
 const monsterContainer = document.getElementById("monster-container");
 
-const initialMonsters = [
-  { id: 1, name: "Maurice", priceInCents: 7999, age: 823, inCart: false },
-  { id: 2, name: "Jorrild", priceInCents: 1950, age: 220, inCart: false },
-  { id: 3, name: "Frankie", priceInCents: 10890, age: 171, inCart: false },
-  { id: 4, name: "Kristy", priceInCents: 8799, age: 142, inCart: false }
-];
-
-const formatDollars = cents => {
-  return (cents / 100).toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD"
-  });
-};
-
 initialMonsters.forEach(monster => {
   console.log("HELLO MONSTERS");
   const monsterBox = document.createElement("DIV");
