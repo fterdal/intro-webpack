3f1363d00dcae7be79707262e5679547b7d95f49
Finn <finnterdal@gmail.com>
2020-03-16 01:37:12 -0500

    adding monsters to the div

---
+++ public/petsdata.js
@@ -0,0 +1,6 @@
+const initialMonsters = [
+  { id: 1, name: "Maurice", priceInCents: 7999, age: 823, inCart: false },
+  { id: 2, name: "Jorrild", priceInCents: 1950, age: 220, inCart: false },
+  { id: 3, name: "Frankie", priceInCents: 10890, age: 171, inCart: false },
+  { id: 4, name: "Kristy", priceInCents: 8799, age: 142, inCart: false }
+];
